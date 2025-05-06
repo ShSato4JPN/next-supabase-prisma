e01cdb9383afd4fe73bb2ae28338e1f18b8d043d
ShSato4JPN <shsato4.private.1221@gmail.com>
2025-05-06 16:08:09 +0900

    feat: first commit

---
+++ eslint.config.mjs
@@ -0,0 +1,64 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript"],
+    rules: {
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "parent",
+            "sibling",
+            "index",
+            "object",
+            "type",
+          ],
+          pathGroups: [
+            {
+              pattern: "{react,react-dom/**,react-router-dom}",
+              group: "builtin",
+              position: "before",
+            },
+            {
+              pattern: "@src/**",
+              group: "parent",
+              position: "before",
+            },
+          ],
+          pathGroupsExcludedImportTypes: ["builtin"],
+          alphabetize: {
+            order: "asc",
+          },
+          "newlines-between": "always",
+        },
+      ],
+      "@typescript-eslint/consistent-type-imports": [
+        "error",
+        { prefer: "type-imports" },
+      ],
+      "no-unused-vars": [
+        "error",
+        {
+          vars: "all",
+          args: "all",
+          argsIgnorePattern: "^_",
+        },
+      ],
+    },
+  }),
+];
+
+export default eslintConfig;
